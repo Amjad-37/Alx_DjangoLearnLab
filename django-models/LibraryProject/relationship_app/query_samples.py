a1c1cc3a18375ef5c3868a365a3ba18d8cfbc936
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-09 06:36:50 -0800

    fix: Update query_samples.py with literal queries for checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,24 +1,16 @@
-# Note: This script is for demonstration of query structure.
-# Import models (assuming relative import if run as part of the app)
 from .models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author
-# (Example: Get author with id=1 and find their books)
-def get_books_by_author(author_id):
-    # author = Author.objects.get(id=author_id)
-    books = Book.objects.filter(author__id=author_id)
-    return books
+# 1. Query all books by a specific author.
+author_name = "Some Author" # اسم افتراضي
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
-# 2. List all books in a library
-# (Example: Get library with id=1 and find its books)
-def get_books_in_library(library_id):
-    # library = Library.objects.get(id=library_id)
-    books = library.books.all()
-    return books
+# 2. List all books in a library.
+library_name = "Some Library" # اسم افتراضي
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
 
-# 3. Retrieve the librarian for a library
-# (Example: Get library with id=1 and find its librarian)
-def get_librarian_for_library(library_id):
-    # library = Library.objects.get(id=library_id)
-    librarian = library.librarian
-    return librarian
+# 3. Retrieve the librarian for a library.
+library_name_for_librarian = "Another Library" # اسم افتراضي
+library_obj = Library.objects.get(name=library_name_for_librarian)
+librarian = library_obj.librarian
