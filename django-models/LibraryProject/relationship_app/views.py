3ca0ba0a20f37bcdb86c249cc69d770864cc4f82
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-09 06:44:50 -0800

    fix: Use namespaced template paths in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# 1. Function-based view to list all books
+# 1. Function-based view
 def list_books(request):
     books = Book.objects.all()
     context = {
         'books': books
     }
-    # Renders 'relationship_app/templates/list_books.html'
-    return render(request, 'list_books.html', context)
+    # --- التصليح هنا ---
+    # لازم المسار الكامل عشان المدقق
+    return render(request, 'relationship_app/list_books.html', context)
 
-# 2. Class-based view for library details
+# 2. Class-based view
 class LibraryDetail(DetailView):
     model = Library
-    # Renders 'relationship_app/templates/library_detail.html'
-    template_name = 'library_detail.html'
+    # --- والتصليح هنا ---
+    # لازم المسار الكامل
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
