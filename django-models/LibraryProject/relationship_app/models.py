251c74c4559364738d5a74972dd78937e6eb5047
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-09 07:07:09 -0800

    fix: Complete rebuild of django-models project from scratch

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,32 +1,24 @@
 from django.db import models
 
-# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
 
-# Book Model (with ForeignKey to Author)
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.title
 
-# Library Model (with ManyToManyField to Book)
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
-
     def __str__(self):
         return self.name
 
-# Librarian Model (with OneToOneField to Library)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
     def __str__(self):
         return self.name
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,19 +1,13 @@
 from .models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author.
 author_name = "Some Author"
 author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
 
-# 2. List all books in a library.
 library_name = "Some Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
-# 3. Retrieve the librarian for a library.
-# (السطور دي عشان نرضي المدقق)
 library_name_for_librarian = "Another Library"
 library_obj = Library.objects.get(name=library_name_for_librarian)
-
-# ده السطر اللي كان ناقصك
 librarian = Librarian.objects.get(library=library_obj)
