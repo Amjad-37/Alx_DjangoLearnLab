78b2c933481d3d949de6153f0c25f1ea3291025f
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-16 12:26:38 -0800

    fix: Included relationship_app.urls in main project urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # السطر ده هو اللي ناقص عندك
+    # هو ده اللي بيربط المشروع بالـ app
     path('', include('relationship_app.urls')),
 ]
