ca18272e4d32bd48c99013a86d5ba71dde45200f
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-23 00:10:24 -0800

    feat: Add authentication views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
-    # مسار الكتب (Function-based view)
+    # Task 1 URLs
     path('books/', list_books, name='list_books'),
-    
-    # مسار المكتبة (Class-based view)
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Task 2 Authentication URLs
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# فصلناهم عشان الـ checker يشوف Library لوحدها
+# Imports for Task 1
 from .models import Library
 from .models import Book
 
-# 1. Function-based view
+# 1. Function-based view (Task 1)
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
-# 2. Class-based view
+# 2. Class-based view (Task 1)
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# 3. Registration View (Task 2)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
