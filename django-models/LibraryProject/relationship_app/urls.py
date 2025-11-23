484db5f37a624cfa13ba48187e2eb490a6d25cd3
Amjad-37 <amjadmohamed2017@gmail.com>
2025-11-23 00:24:47 -0800

    feat: Implement Role-Based Access Control (RBAC)

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Task 2 Authentication Views
-    # Login View
+    # Task 2 Auth Views
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    # Logout View
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    # Register View
     path('register/', views.register, name='register'),
+
+    # Task 3 Role-Based Views
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 
-# Imports for Task 1
+# Imports for models
 from .models import Library
 from .models import Book
 
-# 1. Function-based view (Task 1)
+# --- Task 1 Views ---
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
-# 2. Class-based view (Task 1)
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-# 3. Registration View (Task 2)
+# --- Task 2 Auth View ---
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -30,3 +30,28 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# --- Task 3: Role-Based Views ---
+
+# Helper functions to check roles
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# The Views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
